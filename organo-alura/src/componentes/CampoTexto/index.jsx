49716f13481896233f0940010b7336bd9bf63b69
leomillan22 <leonardo.amillan@outlook.com>
2024-06-10 12:00:47 -0300

    react hooks

--- organo-alura/src/componentes/CampoTexto/index.jsx
+++ organo-alura/src/componentes/CampoTexto/index.jsx
@@ -1,10 +1,16 @@
 import "./CampoTexto.css"
 
+
+const aoDigitado = (evento) =>{
+    props.aoAlterado(evento.target.value)
+}
+
+
 const CampoTexto = (props) => {
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
